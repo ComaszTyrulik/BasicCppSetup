aeba0ebea6584a84a243471534d410af952856bc
ComaszTyrulik <tomaszcyrulikdevelopment@gmail.com>
2020-12-22 01:16:36 +0100

    WIP

--- conanfile.py
+++ conanfile.py
@@ -5,3 +5,7 @@ class BeastEngine(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "spdlog/[>=1.4.2]", "gtest/[>=1.8.1]", "glm/0.9.9.8"
     generators = "cmake"
+
+    def configure(self):
+        if self.settings.compiler == 'clang':
+            self.settings.compiler.libcxx = 'libstdc++'
