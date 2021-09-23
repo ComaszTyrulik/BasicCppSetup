e7f3751cf8b2cf190806f67543e49d6868e992b4
ComaszTyrulik <tomaszcyrulikdevelopment@gmail.com>
2021-09-23 14:14:20 +0200

    Updated scripts. Removed xxHash from vendors, added CommonUtilities as a vendoer

--- src/Lib/Lib.cpp
+++ src/Lib/Lib.cpp
@@ -1,5 +1,6 @@
 #include "Lib/Lib.h"
 
+#include <Common/IdGenerators/UuId4Generator.h>
 #include <spdlog/spdlog.h>
 
 namespace cps
@@ -7,5 +8,8 @@ namespace cps
     void Lib::HelloLib()
     {
         spdlog::info("Hello Basic Cpp Project!");
+
+        auto idGenerator = ct::UuId4Generator();
+        spdlog::info("New id generated {}", idGenerator.Generate());
     }
 } // namespace cps
